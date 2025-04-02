c921547b18855777520167ccea014fce9840d810
LuisArmandoV <enmitiendavirtual1@gmail.com>
2025-04-02 11:25:25 -0500

    Se adelanto la verificacion del Email del nuevo ususario

--- backoffice/controladores/usuarios.controlador.php
+++ backoffice/controladores/usuarios.controlador.php
@@ -40,6 +40,81 @@ Class ControladorUsuarios{
             $respuesta = ModeloUsuarios::mdlRegistroUsuario($tabla, $datos);
 
             if($respuesta == "ok"){
+
+
+
+
+                /*=============================================
+                    Verificación Correo Electrónico
+                    =============================================*/
+
+                    date_default_timezone_set("America/Bogota");
+
+                    $mail = new PHPMailer;
+
+                    $mail->Charset = "UTF-8";
+
+                    $mail->isMail();
+
+                    $mail->setFrom("info@academyoflife.com", "Academy of Life");
+
+                    $mail->addReplyTo("info@academyoflife.com", "Academy of Life");
+
+                    $mail->Subject  = "Por favor verifique su dirección de correo electrónico";
+
+                    $mail->addAddress($_POST["registroEmail"]);
+
+                    $mail->msgHTML('<div style="width:100%; background:#eee; position:relative; font-family:sans-serif; padding-bottom:40px">
+    
+                        <center>
+                                
+                            <img style="padding:20px; width:10%" src="https://tutorialesatualcance.com/tienda/logo.png">
+
+                        </center>
+
+                        <div style="position:relative; margin:auto; width:600px; background:white; padding:20px">
+                            
+                            <center>
+
+                                <img style="padding:20px; width:15%" src="https://tutorialesatualcance.com/tienda/icon-email.png">
+
+                                <h3 style="font-weight:100; color:#999">VERIFIQUE SU DIRECCIÓN DE CORREO ELECTRÓNICO</h3>
+
+                                <hr style="border:1px solid #ccc; width:80%">
+
+                                <h4 style="font-weight:100; color:#999; padding:0 20px">Para comenzar a usar su cuenta, debe confirmar su dirección de correo electrónico</h4>
+
+                                <a href="'.$ruta.$encriptarEmail.'" target="_blank" style="text-decoration:none">
+                                    
+                                    <div style="line-height:60px; background:#0aa; width:60%; color:white">Verifique su dirección de correo electrónico</div>
+
+                                </a>
+
+                                <br>
+
+                                <hr style="border:1px solid #ccc; width:80%">
+
+                                <h5 style="font-weight:100; color:#999">Si no se inscribió en esta cuenta, puede ignorar este correo electrónico y eliminarlo.</h5>
+
+                            </center>   
+
+                        </div>
+
+                    </div>');
+                            
+                    $envio = $mail->Send();
+
+                    if(!$envio){
+
+
+
+
+
+
+
+
+
+                
                 echo '<script>
                     swal({
                         type:"success",
